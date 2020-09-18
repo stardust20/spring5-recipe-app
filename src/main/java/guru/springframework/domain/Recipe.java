9d9f3fb5db8846229b9339c721cb0327ec3e7abb
Josué <josue.boix@mytaxi.com>
2020-09-18 16:45:34 +0200

    Added category and relationship with recipe

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -8,7 +8,10 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -38,6 +41,14 @@ public class Recipe
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @ManyToMany
+    @JoinTable(
+        name = "recipe_category",
+        joinColumns = @JoinColumn(name = "recipe_id"),
+        inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories;
 
 
     public void setId(Long id)
@@ -181,5 +192,17 @@ public class Recipe
     public void setDifficulty(Difficulty difficulty)
     {
         this.difficulty = difficulty;
+    }
+
+
+    public Set<Category> getCategories()
+    {
+        return categories;
+    }
+
+
+    public void setCategories(Set<Category> categories)
+    {
+        this.categories = categories;
     }
 }
